be64d93389ebd367fa90cd5610d7686c0049747d
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 20:05:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть чотири цифри:");
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        int number4 = Convert.ToInt32(Console.ReadLine());
+
+        int result = number1 * 1000 + number2 * 100 + number3 * 10 + number4;
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
+
